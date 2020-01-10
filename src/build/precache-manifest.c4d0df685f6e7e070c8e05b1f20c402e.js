b752cbdd7f1c833f2241a3897ce2d529805e82c6
aymen eljamani <eljamani.aej@gmail.com>
2020-01-10 22:13:25 +0800

    First commit

---
+++ src/build/precache-manifest.c4d0df685f6e7e070c8e05b1f20c402e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6a578b0147c6307fd69e5fddec34c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3752847e5feabf20bf9",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "250e503586bd9a50e232",
+    "url": "/static/js/2.99b653d7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.99b653d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3752847e5feabf20bf9",
+    "url": "/static/js/main.8a609dc8.chunk.js"
+  },
+  {
+    "revision": "40987e1b4e958ee40275",
+    "url": "/static/js/runtime-main.6ba770b8.js"
+  }
+]);
